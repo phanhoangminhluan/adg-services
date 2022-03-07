9415f7f77f6b1b8bda88b4ce15efd84a128924fd
phanhoangminhluan <phanhoangminhluan@gmail.com>
2022-03-07 18:49:06 +0700

    enable spring boot for adg scheduler

---
+++ adg-scheduler/src/main/java/com/adg/scheduler/AdgSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.adg.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdgSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdgSchedulerApplication.class, args);
+    }
+
+}
