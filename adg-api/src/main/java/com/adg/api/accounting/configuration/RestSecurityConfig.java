f972ce306d25eaa192ed80da8189b3345f99a747
minhluan_amanotes <phanhoangminhluan@gmail.com>
2022-05-21 00:48:49 +0700

    sync order

--- adg-api/src/main/java/com/adg/api/accounting/configuration/RestSecurityConfig.java
+++ adg-api/src/main/java/com/adg/api/accounting/configuration/RestSecurityConfig.java
@@ -1,20 +1,20 @@
-package com.adg.api.accounting.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * @author Minh-Luan H. Phan
- * Created on: 2022.05.21 00:34
- */
-@Configuration
-public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**")
-                .permitAll();
-    }
-
-}
+//package com.adg.api.accounting.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author Minh-Luan H. Phan
+// * Created on: 2022.05.21 00:34
+// */
+//@Configuration
+//public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/**")
+//                .permitAll();
+//    }
+//
+//}
